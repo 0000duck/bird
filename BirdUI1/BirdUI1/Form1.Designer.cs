1c1c1987d21a10679240ffa0f4008eb4bc01273c
Dushan kumarasinghe <dushan.14@cse.mrt.ac.lk>
2017-02-21 18:55:22 +0530

    adding ganttChart+UI

---
+++ BirdUI1/BirdUI1/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace BirdUI1
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.closePic = new System.Windows.Forms.PictureBox();
+            this.topTab = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.closePic)).BeginInit();
+            this.topTab.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(147)))), ((int)(((byte)(176)))));
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(47, 45);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // closePic
+            // 
+            this.closePic.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(147)))), ((int)(((byte)(176)))));
+            this.closePic.Image = ((System.Drawing.Image)(resources.GetObject("closePic.Image")));
+            this.closePic.Location = new System.Drawing.Point(751, 4);
+            this.closePic.Name = "closePic";
+            this.closePic.Size = new System.Drawing.Size(36, 38);
+            this.closePic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.closePic.TabIndex = 1;
+            this.closePic.TabStop = false;
+            this.closePic.Click += new System.EventHandler(this.closePic_Click);
+            this.closePic.MouseHover += new System.EventHandler(this.closePic_MouseHover);
+            // 
+            // topTab
+            // 
+            this.topTab.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(147)))), ((int)(((byte)(176)))));
+            this.topTab.Controls.Add(this.closePic);
+            this.topTab.Controls.Add(this.pictureBox2);
+            this.topTab.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topTab.Location = new System.Drawing.Point(0, 0);
+            this.topTab.Name = "topTab";
+            this.topTab.Size = new System.Drawing.Size(836, 49);
+            this.topTab.TabIndex = 3;
+            this.topTab.MouseDown += new System.Windows.Forms.MouseEventHandler(this.topTab_MouseDown_1);
+            this.topTab.MouseMove += new System.Windows.Forms.MouseEventHandler(this.topTab_MouseMove_1);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(151)))), ((int)(((byte)(181)))));
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.topTab);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(836, 78);
+            this.panel2.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(151)))), ((int)(((byte)(181)))));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(174, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Ivory;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(0, 78);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 442);
+            this.panel3.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(200, 78);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(636, 442);
+            this.panel1.TabIndex = 6;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint_1);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 520);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "mainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mainForm_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.mainForm_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.closePic)).EndInit();
+            this.topTab.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox closePic;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel topTab;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ BirdUI1/BirdUI1/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BirdUI1
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        /// Dragable tab for form
+        private Point MouseDownLocation;
+
+       
+        private void topTab_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                MouseDownLocation = e.Location;
+            }
+        }
+
+        private void topTab_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                this.Left = e.X + this.Left - MouseDownLocation.X;
+                this.Top = e.Y + this.Top - MouseDownLocation.Y;
+            }
+        }
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+        /// Dragable tab for form
+
+
+        private void closePic_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void closePic_MouseHover(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        #region 'Resize'
+
+        bool onFullScreen;
+        bool maximized;
+        bool on_MinimumSize;
+        short minimumWidth = 350;
+        short minimumHeight = 26;
+        short borderSpace = 20;
+        short borderDiameter = 3;
+
+        bool onBorderRight;
+        bool onBorderLeft;
+        bool onBorderTop;
+        bool onBorderBottom;
+        bool onCornerTopRight;
+        bool onCornerTopLeft;
+        bool onCornerBottomRight;
+        bool onCornerBottomLeft;
+
+        bool movingRight;
+        bool movingLeft;
+        bool movingTop;
+        bool movingBottom;
+        bool movingCornerTopRight;
+        bool movingCornerTopLeft;
+        bool movingCornerBottomRight;
+        bool movingCornerBottomLeft;
+
+        private void startResizer()
+        {
+            if (movingRight)
+            {
+                this.Width = Cursor.Position.X - this.Location.X;
+            }
+
+            else if (movingLeft)
+            {
+                this.Width = ((this.Width + this.Location.X) - Cursor.Position.X);
+                this.Location = new Point(Cursor.Position.X, this.Location.Y);
+            }
+
+            else if (movingTop)
+            {
+                this.Height = ((this.Height + this.Location.Y) - Cursor.Position.Y);
+                this.Location = new Point(this.Location.X, Cursor.Position.Y);
+            }
+
+            else if (movingBottom)
+            {
+                this.Height = (Cursor.Position.Y - this.Location.Y);
+            }
+
+            else if (movingCornerTopRight)
+            {
+                this.Width = (Cursor.Position.X - this.Location.X);
+                this.Height = ((this.Location.Y - Cursor.Position.Y) + this.Height);
+                this.Location = new Point(this.Location.X, Cursor.Position.Y);
+            }
+
+            else if (movingCornerTopLeft)
+            {
+                this.Width = ((this.Width + this.Location.X) - Cursor.Position.X);
+                this.Location = new Point(Cursor.Position.X, this.Location.Y);
+                this.Height = ((this.Height + this.Location.Y) - Cursor.Position.Y);
+                this.Location = new Point(this.Location.X, Cursor.Position.Y);
+            }
+
+            else if (movingCornerBottomRight)
+            {
+                this.Size = new Size(Cursor.Position.X - this.Location.X, Cursor.Position.Y - this.Location.Y);
+            }
+
+            else if (movingCornerBottomLeft)
+            {
+                this.Width = ((this.Width + this.Location.X) - Cursor.Position.X);
+                this.Height = (Cursor.Position.Y - this.Location.Y);
+                this.Location = new Point(Cursor.Position.X, this.Location.Y);
+            }
+        }
+
+        private void stopResizer()
+        {
+            movingRight = false;
+            movingLeft = false;
+            movingTop = false;
+            movingBottom = false;
+            movingCornerTopRight = false;
+            movingCornerTopLeft = false;
+            movingCornerBottomRight = false;
+            movingCornerBottomLeft = false;
+            this.Cursor = Cursors.Default;
+            System.Threading.Thread.Sleep(300);
+            on_MinimumSize = false;
+        }
+
+        private void mainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            startResizer();
+        }
+
+        private void mainForm_MouseUp(object sender, MouseEventArgs e)
+        {
+            stopResizer();
+        }
+
+        #endregion
+    }
+}
